383794eb7bf46321a1a663d0c119740e7e4f0be7
Alerrando Breno <alerrando2@gmail.com>
2022-07-18 15:14:54 -0300

    Adicionando a logo

--- Contador-Simples/src/App.tsx
+++ Contador-Simples/src/App.tsx
@@ -7,30 +7,47 @@ export function App() {
     setCont(cont + 1)
   }
 
-  function Discrementar(){
+  function Resetar(){
+    setCont(0)
+  }
+
+  function Decrementar(){
     setCont(cont - 1)
   }
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="gap-4">
-        <button 
-          onClick={Incrementar}
-          className="py-1 px-10 mr-5 text-zinc-100 bg-blue-700 hover:bg-blue-500"
-        >
-          +
-        </button>
-        <button
-          onClick={Discrementar}
-          className="py-1 px-10 text-zinc-100 bg-blue-700 hover:bg-blue-500"
-        >
-          -
-        </button>
+    <div className="h-56 w-full flex justify-center items-start">
+      <div className="text-center">
+
+        <div className="text-zinc-100 mt-10">
+          <h1 className="text-6xl mb-16">Contador</h1>
+          <span className="text-6xl text-teal-400">{cont}</span>
+        </div>
+
+        <div className="gap-4 mt-16 text-xl font-bold">
+          <button 
+            onClick={Decrementar}
+            className="py-3 px-12 mr-5 text-zinc-100 border-4 border-solid rounded-xl bg-transparent border-red-600 hover:border-blue-700"
+          >
+            Decrementar
+          </button>
+
+          <button 
+            onClick={Resetar}
+            className="py-3 px-12 mr-5 text-zinc-100 border-4 border-solid rounded-xl bg-transparent border-red-600 hover:border-blue-700"
+          >
+            Resetar
+          </button>
+
+          <button
+            onClick={Incrementar}
+            className="py-3 px-12 mr-5 text-zinc-100 border-4 border-solid rounded-xl bg-transparent border-red-600 hover:border-blue-700"
+          >
+            Aumentar
+          </button>
+        </div>
       </div>
 
-      <div className="absolute text-xl mt-20 text-zinc-100">
-        {cont}
-      </div>
     </div>
   )
 }
